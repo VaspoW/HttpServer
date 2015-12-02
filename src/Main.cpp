a95b7c201714c0529043f8e2ff8f71edbe2888f5
manthanrtilva <manthanrtilva@gmail.com>
2015-12-02 11:49:37 +0530

    initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+int main(int argc,char** argv)
+{
+	//initialize app
+	
+	//create socket
+	
+	//set address
+	
+	//bind socket
+	
+	//listen socket
+	
+	//accept on socket
+	
+		//on request
+		
+		//create new thread to handle connection
+		
+		//read data from connection
+		
+		//get version
+			//validate version
+		//get request type
+			//validate request type
+		//respond request
+		
+	return 0;
+}
